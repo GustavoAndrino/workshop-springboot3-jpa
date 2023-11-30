fac9b3fa9011c39c4091842befe989ae0b00b288
Gustavo Andrino <gustavohtandrino@gmail.com>
2023-11-30 13:08:50 -0300

    Find by name + Order, Instant, ISO 8601 part1

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.educandoweb.course.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
-	
-	
 
+	List<User> findByNameContains(String name);
 }
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,20 +1,50 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity <List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/findid/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+
+	@GetMapping(value = "/findname/{name}")
+	public ResponseEntity<?> findByNameContains(@PathVariable String name){
+		List<User> obj = service.findByNameContains(name);	
+		
+		if(obj.isEmpty()) {
+			
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User \'" + name + "\' not found");
+		}
+		else {
+			return ResponseEntity.ok(obj);
+		}
+		
 	}
 	
 }
